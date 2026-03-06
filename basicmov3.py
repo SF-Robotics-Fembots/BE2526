a752e25371a64bcfbb42787d6534596f247fe995
gabby rosario <25Rosario@stfrancishs.org>
2026-03-05 16:39:50 -0800

    new file

---
+++ basicmov3.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set the GPIO mode (BOARD)
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin controls the pump via the relay module
+RELAY_PIN = 6
+RELAY_PIN2 = 5
+# Set the relay pin as an output pin
+GPIO.setup(RELAY_PIN2, GPIO.OUT)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+
+
+
+try:
+    # Run the loop function forever
+    GPIO.output(RELAY_PIN2, GPIO.LOW)
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+    while True:
+        # Turn the relay on to turn on the pump
+        print("Turning on")
+        GPIO.output(RELAY_PIN, GPIO.HIGH)
+        GPIO.output(RELAY_PIN2, GPIO.LOW)
+        time.sleep(5)
+        
+        
+        # Turn the relay off to turn off the pump
+        print("Turning off")
+        GPIO.output(RELAY_PIN, GPIO.LOW)
+        GPIO.output(RELAY_PIN2, GPIO.HIGH)
+        time.sleep(5)
+        
+
+except KeyboardInterrupt:
+    #press Ctrl+C, clean up the config
+    GPIO.cleanup()
